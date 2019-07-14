aa80036448f17bb7bc902ef60a22ab3325675c4e
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-14 16:28:50 -0700

    Added test: ambassador_mode_1_adds_1_card_to_supply.

--- projects/shawrob/dominion/unittest3.c
+++ projects/shawrob/dominion/unittest3.c
@@ -40,22 +40,74 @@ testResult ambassador_mode_1_removes_1_card_from_hand(struct gameState gameState
   struct gameState testState;
   memcpy(&testState, &gameState, sizeof(struct gameState));
   memset(testTitle, '\0', 256);
-  if(!NOISY_TEST) strncpy(testTitle, "Playing Ambassador returning 1 card removes 1 from player's hand.\n", 255);
+  if(!NOISY_TEST) strncpy(testTitle, "Playing Ambassador returning 1 card removes 1 from player's hand.", 255);
 
   int copperBefore = 0;
   int copperAfter  = 0;
 
   for(int i = 0; i < testState.handCount[0]; i++)
     if(testState.hand[0][i] == copper) ++copperBefore;
-  noisyprint("copperBefore = %d\n", copperBefore);
+  noisyprint("\tcopperBefore = %d\n", copperBefore);
 
   playCard(0, 1, 0, 1, &testState);
 
   for(int i = 0; i < testState.handCount[0]; i++)
     if(testState.hand[0][i] == copper) ++copperAfter;
-  noisyprint("copperAfter = %d\n", copperAfter);
+  noisyprint("\tcopperAfter = %d\n", copperAfter);
 
   if(copperAfter == copperBefore - 1)
+    return success;
+  else
+    return failure;
+}
+
+testResult ambassador_mode_2_removes_2_cards_from_hand(struct gameState gameState)
+{
+  noisyprint("Playing Ambassador returning 2 cards removes 2 cards from player's hand:\n");
+  struct gameState testState;
+  memcpy(&testState, &gameState, sizeof(struct gameState));
+  memset(testTitle, '\0', 256);
+  if(!NOISY_TEST) strncpy(testTitle, "Playing Ambassador returning 2 cards removes 2 cards from player's hand.", 255);
+
+  int copperBefore = 0;
+  int copperAfter  = 0;
+
+  for(int i = 0; i < testState.handCount[0]; i++)
+    if(testState.hand[0][i] == copper) ++copperBefore;
+  noisyprint("\tcopperBefore = %d\n", copperBefore);
+
+  playCard(0, 2, 0, 1, &testState);
+
+  for(int i = 0; i < testState.handCount[0]; i++)
+    if(testState.hand[0][i] == copper) ++copperAfter;
+  noisyprint("\tcopperAfter = %d\n", copperAfter);
+
+  if(copperAfter == copperBefore - 1)
+    return success;
+  else
+    return failure;
+}
+
+testResult ambassador_mode_1_adds_1_card_to_supply(struct gameState gameState)
+{
+  noisyprint("Playing Ambassador returning 1 card adds 1 to supply:\n");
+  struct gameState testState;
+  memcpy(&testState, &gameState, sizeof(struct gameState));
+  memset(testTitle, '\0', 256);
+  if(!NOISY_TEST) strncpy(testTitle, "Playing Ambassador returning 1 card adds 1 to supply.", 255);
+
+  int supplyBefore = 0;
+  int supplyAfter  = 0;
+
+  supplyBefore = testState.supplyCount[copper];
+  noisyprint("\tsupplyBefore = %d\n", supplyBefore);
+
+  playCard(0, 1, 0, 1, &testState);
+
+  supplyAfter = testState.supplyCount[copper];
+  noisyprint("\tsupplyAfter = %d\n", supplyAfter);
+
+  if(supplyAfter == supplyBefore + 1)
     return success;
   else
     return failure;
@@ -79,6 +131,8 @@ int main(int argc, char *argv[]){
 
   assert_true( first_test_returns_success(gameState) );
   assert_true( ambassador_mode_1_removes_1_card_from_hand(gameState) );
+  assert_true( ambassador_mode_2_removes_2_cards_from_hand(gameState) );
+  assert_true( ambassador_mode_1_adds_1_card_to_supply(gameState) );
 
   printf("====END TEST SUITE FOR CARD: AMBASSADOR====\n\n");
   return 0;
