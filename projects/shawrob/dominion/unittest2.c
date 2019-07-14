f7e28ab33c1197fb8e08b6c693b33186609fdef1
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-14 13:53:36 -0700

    Began creating tests for Minion.

---
+++ projects/shawrob/dominion/unittest2.c
@@ -0,0 +1,51 @@
+#include "dominion_helpers.h"   // "dominion.h" is included within this header.
+#include <stdio.h>              // printf
+#include <string.h>             // memcpy
+
+#define testResult int
+#define success 1
+#define failure 0
+
+#define bool  int
+#define true  1
+#define false 0
+
+bool NOISY_TEST = false;
+#define noisyprint if(NOISY_TEST) printf
+
+char testTitle[256];
+
+void assert_true(testResult result)
+{
+  if (result == success)
+    printf("+ PASS\t%s\n", testTitle);
+  else
+    printf("- FAIL\t%s\n", testTitle);
+}
+
+testResult first_test(struct gameState gameState)
+{
+  return failure;
+}
+
+int main(int argc, char *argv[]){
+  if (argc > 1 && strcmp(argv[1], "-n") == 0) NOISY_TEST = true;
+
+  printf("\n===BEGIN TEST SUITE FOR CARD: BARON===\n");
+  if(!NOISY_TEST) printf("For noisy test: %s -n\n\n", argv[0]);
+
+  struct gameState gameState;
+
+  const int NUM_PLAYERS = 2;
+  const int RNG_SEED    = 1000;
+
+  int kingdom[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
+
+  initializeGame(NUM_PLAYERS, kingdom, RNG_SEED, &gameState);
+  gameState.hand[0][0] = minion;
+
+  assert_true( first_test(gameState) );
+
+  printf("====END TEST SUITE FOR CARD: BARON====\n\n");
+  return 0;
+}
