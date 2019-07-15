bb56468b87cf7c936b7211a3553bf5bf89746d38
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-14 20:14:01 -0700

    Added test: wins marked correctly.

--- projects/shawrob/dominion/cardtest4.c
+++ projects/shawrob/dominion/cardtest4.c
@@ -57,6 +57,31 @@ testResult ties_marked_correctly(struct gameState gameState)
     return failure;
 }
 
+testResult winner_marked_correctly(struct gameState gameState)
+{
+  noisyprint("Winner marked correctly:\n");
+  struct gameState testState;
+  memcpy(&testState, &gameState, sizeof(struct gameState));
+  memset(testTitle, '\0', 256);
+
+  testState.deck[1][0] = province;
+  if(!NOISY_TEST) strncpy(testTitle, "Winner marked correctly.", 255);
+
+  int players[2] = {0};
+  for(int i = 0; i < 2; i++)
+    noisyprint("\tplayer[%d] = %d\n", i, players[i]);
+
+  getWinners(players, &testState);
+
+  for(int i = 0; i < 2; i++)
+    noisyprint("\tplayer[%d] = %d\n", i, players[i]);
+
+  if(players[1] == 1 && players[0] == 0)
+    return success;
+  else
+    return failure;
+}
+
 int main(int argc, char *argv[]){
   if (argc > 1 && strcmp(argv[1], "-n") == 0) NOISY_TEST = true;
 
@@ -74,6 +99,7 @@ int main(int argc, char *argv[]){
 
   assert_true( first_test_returns_success(gameState) );
   assert_true( ties_marked_correctly(gameState) );
+  assert_true( winner_marked_correctly(gameState) );
 
   printf("====END TEST SUITE FOR FUNCTION: GETWINNERS====\n\n");
   return 0;
