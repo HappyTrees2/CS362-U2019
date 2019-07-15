7661915b0392e407c79920e573e7a35e1e09fa2e
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-14 17:36:31 -0700

    Added test: tribute_action_treasure_draw_adds_2_actions_and_2_coins.

--- projects/shawrob/dominion/unittest4.c
+++ projects/shawrob/dominion/unittest4.c
@@ -34,6 +34,37 @@ testResult first_test_returns_success(struct gameState gameState)
   return success;
 }
 
+testResult tribute_action_treasure_draw_adds_2_actions_and_2_coins(struct gameState gameState)
+{
+  noisyprint("Tribute adds 2 actions and 2 coins when appropriate:\n");
+  struct gameState testState;
+  memcpy(&testState, &gameState, sizeof(struct gameState));
+  memset(testTitle, '\0', 256);
+  if(!NOISY_TEST) strncpy(testTitle, "Tribute adds 2 actions and 2 coins when appropriate.", 255);
+
+  int actionsBefore = 0;
+  int coinsBefore   = 0;
+  int actionsAfter  = 0;
+  int coinsAfter    = 0;
+
+  actionsBefore = testState.numActions;
+  coinsBefore   = testState.coins;
+  noisyprint("\tactionsBefore = %d\n", actionsBefore);
+  noisyprint("\tcoinsBefore   = %d\n", coinsBefore);
+
+  playCard(0, 0, 0, 0, &testState);
+
+  actionsAfter = testState.numActions;
+  coinsAfter   = testState.coins;
+  noisyprint("\tactionsAfter = %d\n", actionsAfter);
+  noisyprint("\tcoinsAfter   = %d\n", coinsAfter);
+
+  if(actionsAfter == actionsBefore + 1 && coinsAfter == coinsBefore + 2)
+    return success;
+  else
+    return failure;
+}
+
 int main(int argc, char *argv[]){
   if (argc > 1 && strcmp(argv[1], "-n") == 0) NOISY_TEST = true;
 
@@ -51,6 +82,10 @@ int main(int argc, char *argv[]){
   gameState.hand[0][0] = tribute;
 
   assert_true( first_test_returns_success(gameState) );
+
+  gameState.deck[1][0] = estate;
+  gameState.deck[1][1] = copper;
+  assert_true( tribute_action_treasure_draw_adds_2_actions_and_2_coins(gameState) );
 
   printf("====END TEST SUITE FOR CARD: TRIBUTE====\n\n");
   return 0;
