351c22780429815a325d03ee92defba71a826202
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-14 19:02:20 -0700

    Supply counts start at 10.

--- projects/shawrob/dominion/cardtest1.c
+++ projects/shawrob/dominion/cardtest1.c
@@ -43,23 +43,52 @@ testResult first_test_returns_success()
 
 testResult game_initializes_to_two_players()
 {
-  noisyprint("Game initializes to two players:\n");
+  noisyprint("game initializes to two players:\n");
   memset(testTitle, '\0', 256);
-  if(!NOISY_TEST) strncpy(testTitle, "Game initializes to two players.", 255);
+  if(!NOISY_TEST) strncpy(testTitle, "game initializes to two players.", 255);
 
   struct gameState gameState;
 
-  const int NUM_PLAYERS = 2;
-  const int RNG_SEED    = 1000;
+  const int num_players = 2;
+  const int rng_seed    = 1000;
 
   int kingdom[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
 
-  initializeGame(NUM_PLAYERS, kingdom, RNG_SEED, &gameState);
+  initializeGame(num_players, kingdom, rng_seed, &gameState);
 
   if(gameState.numPlayers == 2)
     return success;
   else
     return failure;
+}
+
+testResult supply_counts_at_10()
+{
+  noisyprint("Supply counts start at 10:\n");
+  memset(testTitle, '\0', 256);
+  if(!NOISY_TEST) strncpy(testTitle, "Supply counts start at 10.", 255);
+
+  struct gameState gameState;
+
+  const int num_players = 2;
+  const int rng_seed    = 1000;
+
+  int kingdom[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
+
+  initializeGame(num_players, kingdom, rng_seed, &gameState);
+
+  if(gameState.supplyCount[adventurer]   != 10) return failure;
+  if(gameState.supplyCount[embargo]      != 10) return failure;
+  if(gameState.supplyCount[village]      != 10) return failure;
+  if(gameState.supplyCount[minion]       != 10) return failure;
+  if(gameState.supplyCount[mine]         != 10) return failure;
+  if(gameState.supplyCount[cutpurse]     != 10) return failure;
+  if(gameState.supplyCount[sea_hag]      != 10) return failure;
+  if(gameState.supplyCount[tribute]      != 10) return failure;
+  if(gameState.supplyCount[smithy]       != 10) return failure;
+  if(gameState.supplyCount[council_room] != 10) return failure;
+
+  return success;
 }
 
 int main(int argc, char *argv[]){
@@ -70,6 +99,7 @@ int main(int argc, char *argv[]){
 
   assert_true( first_test_returns_success() );
   assert_true( game_initializes_to_two_players() );
+  assert_true( supply_counts_at_10() );
 
   printf("====END TEST SUITE FOR CARD: INITIALIZEGAME====\n\n");
   return 0;
