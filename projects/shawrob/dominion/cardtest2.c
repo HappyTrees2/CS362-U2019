f4325622f46aace9df04a02b2be6beddbabcc3b4
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-14 19:13:13 -0700

    Began creating tests for shuffle.

---
+++ projects/shawrob/dominion/cardtest2.c
@@ -0,0 +1,54 @@
+#include "dominion_helpers.h"   // "dominion.h" is included within this header.
+#include <stdio.h>              // printf
+#include <string.h>             // memcpy
+
+#define testResult int
+#define success 1
+#define failure 0
+
+#define bool  int
+#define true  1
+#define false 0
+
+bool NOISY_TEST = false;
+#define noisyprint if(NOISY_TEST) printf
+
+char testTitle[256];
+
+void assert_true(testResult result)
+{
+  if (result == success)
+    printf("+ PASS\t%s\n", testTitle);
+  else
+    printf("- FAIL\t%s\n", testTitle);
+}
+
+testResult first_test_returns_success()
+{
+  noisyprint("First test returns success:\n");
+  memset(testTitle, '\0', 256);
+  if(!NOISY_TEST) strncpy(testTitle, "First test returns success.", 255);
+
+  struct gameState gameState;
+
+  const int NUM_PLAYERS = 2;
+  const int RNG_SEED    = 1000;
+
+  int kingdom[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
+
+  initializeGame(NUM_PLAYERS, kingdom, RNG_SEED, &gameState);
+
+  return success;
+}
+
+int main(int argc, char *argv[]){
+  if (argc > 1 && strcmp(argv[1], "-n") == 0) NOISY_TEST = true;
+
+  printf("\n===BEGIN TEST SUITE FOR CARD: INITIALIZEGAME===\n");
+  if(!NOISY_TEST) printf("For noisy test: %s -n\n\n", argv[0]);
+
+  assert_true( first_test_returns_success() );
+
+  printf("====END TEST SUITE FOR CARD: INITIALIZEGAME====\n\n");
+  return 0;
+}
