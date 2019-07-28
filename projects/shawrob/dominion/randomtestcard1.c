6efd6bc0789148221fd3259b570cc224bbbe222b
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-07-27 19:30:15 -0700

    Updated code with comments for clarity.

--- projects/shawrob/dominion/randomtestcard1.c
+++ projects/shawrob/dominion/randomtestcard1.c
@@ -1,3 +1,4 @@
+// Create alias 'bool' for primitive type 'int' to improve code clarity:
 #define bool  int
 #define true  1
 #define false 0
@@ -5,20 +6,23 @@
 #include <stdio.h>
 #include <string.h>
 
-bool NOISY_TEST = false;
+bool NOISY_TEST = false; // If true, display extra information during execution (normally used for debugging).
 
-bool check_switch_noisyTest(int argc, char *argv[]);
+bool check_switch_noisyTest(int argc, char *argv[]); // Return true if flag '-n' set.
 
 int main(int argc, char *argv[])
 {
   NOISY_TEST = check_switch_noisyTest(argc, argv);
   printf("===BEGIN random tests for card: BARON===\n");
+  // INSERT TESTS HERE //
   printf("===END   random tests for card: BARON===\n");
   return 0;
 }
 
 bool check_switch_noisyTest(int argc, char *argv[])
 {
+  // Currently, will only return true if EXACTLY argv[1] is set to '-n'.
+  // TODO: Update this to function like UNIX builtin flags (set anywhere and in any combination).
   if (argc > 1 && strcmp(argv[1], "-n") == 0)
   {
     printf("NOISY TEST\n");
