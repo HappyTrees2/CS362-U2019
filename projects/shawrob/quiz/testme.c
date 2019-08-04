f8305a54070c808a5d9b0527096aab7a9b9b765e
Robert Sargeant Shaw III (Ross Shaw) <shawrob@oregonstate.edu>
2019-08-04 14:02:38 -0700

    Completed inputString() implementation.

--- projects/shawrob/quiz/testme.c
+++ projects/shawrob/quiz/testme.c
@@ -12,8 +12,15 @@ char inputChar()
 
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+  char returnString[5];
+  int lower = 97;
+  int upper = 122;
+  int currentCharacter;
+  for (currentCharacter = 0; currentCharacter < 5; currentCharacter++)
+    returnString[currentCharacter] = (rand() % (upper - lower +1)) + lower;
+  returnString[5] = '\0';
+
+    return returnString;
 }
 
 void testme()
@@ -32,18 +39,12 @@ void testme()
     if (c == '[' && state == 0) state = 1;
     if (c == '(' && state == 1) state = 2;
     if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
+    if (c == ' ' && state == 3) state = 4;
     if (c == 'a' && state == 4) state = 5;
     if (c == 'x' && state == 5) state = 6;
     if (c == '}' && state == 6) state = 7;
     if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8)
-    {
-      printf("error ");
-      exit(200);
-    }
-    
-    /* TODO: Restore this code once inputChar() is complete.
+    if (c == ']' && state == 8) state = 9;
     if (s[0] == 'r' && s[1] == 'e'
        && s[2] == 's' && s[3] == 'e'
        && s[4] == 't' && s[5] == '\0'
@@ -52,7 +53,6 @@ void testme()
       printf("error ");
       exit(200);
     }
-    */
   }
 }
 
